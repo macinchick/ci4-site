d956b179efe024c6973d25fe62c7c036b54ffbb8
macinchick <cherie.benoit@gmail.com>
2017-08-01 16:25:11 -0700

    Revert "Try moving the the index to root"
    This reverts commit 06afb2d774311171b3837d7668da42331b3f9858.

--- application/Config/App.php
+++ application/Config/App.php
@@ -21,7 +21,7 @@ class App extends BaseConfig
 	| environments.
 	|
 	*/
-	public $baseURL = 'http://ci4-site.dev/';
+	public $baseURL = '';
 
 	/*
 	|--------------------------------------------------------------------------
